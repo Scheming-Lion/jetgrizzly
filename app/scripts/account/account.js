bf562e979cab4eab8f517e0434be2ccf3a7d3bf9
Will Burgo <wburgo999@gmail.com>
2014-10-29 16:34:19 -0700

    (feat) updated account page to display a form the user should fill out

--- app/scripts/account/account.js
+++ app/scripts/account/account.js
@@ -2,15 +2,22 @@
 'use strict';
 // does not do much now, but provide a template/page for user account information
 (function(){
+
 var module = angular.module('jetgrizzlyApp.Account',['ui.router']);
+
 module.config(function($stateProvider) {
   $stateProvider.state('account', {
     url:'/account',
     parent:'app',
     templateUrl:'views/account/account.html',
-    controller:function($scope, user) {
-      $scope.user = user;
-    }
+    controller:"AccountController"
   });
+
 });
+
+module.controller('AccountController', function ($scope, user) {
+  $scope.user = user;
+
+})
+
 })();
